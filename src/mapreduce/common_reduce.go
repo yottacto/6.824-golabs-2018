77ee7f3675dd6a62a8e5214d2636bb2c592f6616
yottacto <yotta.w.cto@gmail.com>
2018-10-28 13:52:27 +0800

    Done common_reduce

--- src/mapreduce/common_reduce.go
+++ src/mapreduce/common_reduce.go
@@ -1,5 +1,11 @@
 package mapreduce
 
+import (
+    "encoding/json"
+    "os"
+    "sort"
+)
+
 func doReduce(
     jobName string, // the name of the whole MapReduce job
     reduceTask int, // which reduce task this is
@@ -44,5 +50,43 @@ func doReduce(
     //
     // Your code here (Part I).
     //
+
+    var kvs []KeyValue
+    for i := 0; i < nMap; i++ {
+        f, err := os.Open(reduceName(jobName, i, reduceTask))
+        check_error(err)
+        defer f.Close()
+
+        dec := json.NewDecoder(f)
+        var kv KeyValue
+        for dec.Decode(&kv) == nil {
+            kvs = append(kvs, kv)
+        }
+        sort.Slice(kvs, func(i, j int) bool {
+            return kvs[i].Key < kvs[j].Key
+        })
+
+        out_file, err := os.Create(outFile)
+        check_error(err)
+        defer out_file.Close()
+
+        enc := json.NewEncoder(out_file)
+
+        for j := 0; j < len(kvs); {
+            count := 0
+            key := kvs[j].Key
+            for k := j; k < len(kvs) && kvs[k].Key == key; k++ {
+                count++
+            }
+            values := make([]string, count)
+            for t := range values {
+                values[t] = kvs[j + t].Value
+            }
+
+            // reduceF func(key string, values []string) string,
+            enc.Encode(&KeyValue{key, reduceF(key, values)})
+            j += count
+        }
+    }
 }
 
